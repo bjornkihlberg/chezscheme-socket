25e5f729b386fa259353cc98b9750a7d10ed1860
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-05-13 15:12:05 +0200

    allow sending bytevectors

--- wsa.scm
+++ wsa.scm
@@ -7,14 +7,28 @@
           receive
           receive-client
           send
+          send-string
           server-listen)
   (import (chezscheme))
-  (define (with-foreign-alloc-ptr size k)
-    (let ([ptr #f])
+  (define (call-with-foreign-alloc-ptr n k)
+    (let ([p #f])
       (dynamic-wind
-        (lambda () (set! ptr (foreign-alloc size)))
-        (lambda () (assert ptr) (k ptr))
-        (lambda () (when ptr (foreign-free ptr))))))
+        (lambda () (set! p (foreign-alloc n)))
+        (lambda () (assert p) (k p))
+        (lambda () (when p (foreign-free p))))))
+  (define (foreign-ptr->bytevector p n)
+    (let ([x (make-bytevector n)])
+      (do ([i 0 (add1 i)])
+          ((>= i n) x)
+        (bytevector-u8-set! x i (foreign-ref 'unsigned-8 p i)))))
+  (define (call-bytevector-with-foreign-ptr bv k)
+    (let ([n (bytevector-length bv)])
+      (call-with-foreign-alloc-ptr
+        n
+        (lambda (p)
+          (do ([i 0 (add1 i)])
+              ((>= i n) (k p n))
+            (foreign-set! 'unsigned-8 p i (bytevector-u8-ref bv i)))))))
   (define chunk-size (make-parameter 1024))
   (define socketBind
     (foreign-procedure "socketBind" (int uptr) int))
@@ -28,14 +42,18 @@
     (foreign-procedure "socketClose" (uptr) int))
   (define socketCleanup
     (foreign-procedure "socketCleanup" () int))
+  (define socketSendString
+    (foreign-procedure "socketSendString" (uptr string) int))
   (define socketSend
-    (foreign-procedure "socketSend" (uptr string) int))
+    (foreign-procedure "socketSend"
+      (uptr uptr unsigned-int)
+      int))
   (define socketReceive
     (foreign-procedure "socketReceive"
       (uptr uptr int uptr)
       int))
   (define (create-server port)
-    (with-foreign-alloc-ptr
+    (call-with-foreign-alloc-ptr
       (foreign-sizeof 'uptr)
       (lambda (ptr)
         (assert (zero? (socketBind port ptr)))
@@ -45,13 +63,13 @@
     (assert (zero? (socketListen server back-log)))
     (void))
   (define (receive-client server)
-    (with-foreign-alloc-ptr
+    (call-with-foreign-alloc-ptr
       (foreign-sizeof 'uptr)
       (lambda (ptr)
         (assert (zero? (socketAccept server ptr)))
         (foreign-ref 'uptr ptr 0))))
   (define (connect server-address port)
-    (with-foreign-alloc-ptr
+    (call-with-foreign-alloc-ptr
       (foreign-sizeof 'uptr)
       (lambda (ptr)
         (assert (zero? (socketConnect server-address port ptr)))
@@ -60,29 +78,24 @@
     (assert (zero? (socketClose socket)))
     (void))
   (define (cleanup) (assert (zero? (socketCleanup))) (void))
-  (define (send socket message)
-    (assert (zero? (socketSend socket message)))
+  (define (send-string socket message)
+    (assert (zero? (socketSendString socket message)))
     (void))
+  (define (send socket bytevector)
+    (call-bytevector-with-foreign-ptr
+      bytevector
+      (lambda (ptr len)
+        (assert (zero? (socketSend socket ptr len)))
+        (void))))
   (define (receive socket)
-    (let ([len (chunk-size)] [buf-ptr #f] [size-ptr #f])
-      (dynamic-wind
-        (lambda ()
-          (set! buf-ptr (foreign-alloc len))
-          (set! size-ptr (foreign-alloc (foreign-sizeof 'int))))
-        (lambda ()
-          (assert buf-ptr)
-          (assert size-ptr)
-          (assert (zero? (socketReceive socket buf-ptr len size-ptr)))
-          (let* ([size (foreign-ref 'int size-ptr 0)]
-                 [raw (make-bytevector size)])
-            (let loop ([i 0])
-              (when (< i size)
-                (bytevector-u8-set!
-                  raw
-                  i
-                  (foreign-ref 'unsigned-8 buf-ptr i))
-                (loop (add1 i))))
-            raw))
-        (lambda ()
-          (when buf-ptr (foreign-free buf-ptr))
-          (when size-ptr (foreign-free size-ptr)))))))
+    (let ([n (chunk-size)])
+      (call-with-foreign-alloc-ptr
+        n
+        (lambda (p)
+          (assert p)
+          (call-with-foreign-alloc-ptr
+            (foreign-sizeof 'int)
+            (lambda (np)
+              (assert np)
+              (assert (zero? (socketReceive socket p n np)))
+              (foreign-ptr->bytevector p (foreign-ref 'int np 0)))))))))
